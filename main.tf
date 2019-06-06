32e9cf7a422dad04487ac7fa9108e6a2c7cfe742
Maciej Rostanski <mrostanski@gmail.com>
2019-06-06 21:49:22 +0200

    fix

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 
 provider "aws" {
   alias = "ireland"
-  region = "${[for x in var.regions: x.region if x.alias == "ireland"])}"
+  region = "${[for x in var.regions: x.region if x.alias == "ireland"]}"
 }
 
 module "vpc_frankfurt" {
